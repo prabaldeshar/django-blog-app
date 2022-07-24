9580be0a493784f723cc5ebff1b2f4fad37892a7
Prabal Deshar <prabaldeshar@gmail.com>
2022-07-24 16:07:20 +0545

    Add: Blog App added with Blge Model

---
+++ website/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    blog_image_url = models.URLField()
+    description = models.TextField()
+    reading_time = models.IntegerField()
+    author_name = models.CharField(max_length=50)
+    author_designation = models.CharField(max_length=100)
+    author_image_url = models.URLField()
+
---
+++ website/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def add_blogs_to_db(request):
+    pass
+
